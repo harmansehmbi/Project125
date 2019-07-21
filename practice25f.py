c6b0fc26456f48b2a57448a27b06004f22a79a5a
harmansehmbi <51370954+harmansehmbi@users.noreply.github.com>
2019-07-21 13:35:31 +0530

    Initial commit

---
+++ practice25f.py
@@ -0,0 +1,17 @@
+import cv2, time
+# import time
+
+video = cv2.VideoCapture(0)
+
+check, frame = video.read()
+
+print("==check==")
+print(check)
+
+print("==frame==")
+print(frame)
+
+# Sleep main thread for 3 secs
+time.sleep(3)
+video.release()
+cv2.destroyAllWindows()
